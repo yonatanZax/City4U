97f2c98d132924e590780a2a94a144a3504804cd
yonatanZax <yona.zax@gmail.com>
2019-05-08 17:19:05 +0300

    Working methods: authUser, addNewUser, getUserQuestion, answerUserQuestion

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-user_db = {}
-
+user_db = {"a":{"uName":'a', "password": 'a'}};
 
 
 
@@ -45,14 +44,16 @@ router.post("/addNewUser",(req,res)=>{
   // var country =
   // var email =
   // var interestList =
-  // var qna =
+  var qna = {"question" : "What's your dog's name","answer":"my dog"}
 
   if(user_db[userName] == null){
 
     const newUser = { uName: userName,
                       password: password,
                       fName: fName,
-                      lName: lName  };
+                      lName: lName,
+                      qANDa: qna
+                    };
 
     user_db[userName] = newUser;
     res.status(201).send('Added');
@@ -65,7 +66,45 @@ router.post("/addNewUser",(req,res)=>{
 });
 
 
+/*  Get - getUserQuestion   */
+router.get('/getUserQuestion/:uName', function(req, res, next) {
+  var params = req.params;
+  var userName = params.uName;
 
+  if(user_db[userName] == null){
+    res.status(400).send('NotExists');
+  }else{
+    var user = user_db[userName];
+    var question = user.qANDa.question;
+    res.status(200).send(question);
+  }
+});
+
+
+
+/*  Post - answerUserQuestion   */
+router.post("/answerUserQuestion",(req,res)=>{
+
+  var userName = req.body.uName;
+  var question = req.body.question;
+  var answer = req.body.answer;
+
+  if(user_db[userName] != null){
+
+    var user = user_db[userName];
+    var qna = user.qANDa;
+    if (qna.answer == answer){
+      res.status(200).send('Correct');
+    }else{
+      res.status(400).send('Incorrect')
+    }
+  }else{
+    res.status(400).send('NotExists')
+  }
+
+
+
+});
 
 
 module.exports = router;
